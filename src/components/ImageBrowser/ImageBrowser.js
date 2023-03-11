a95263547080f04441a3182d5042c604c2b4a50e
Daniel Ruiz <danielruimar@gmail.com>
2023-03-11 09:17:14 +0100

    add style and implement functions

--- src/components/ImageBrowser/ImageBrowser.js
+++ src/components/ImageBrowser/ImageBrowser.js
@@ -2,6 +2,37 @@ import './ImageBrowser.css'
 import React from 'react'
 import Photo from '../Photo/Photo';
 
+const initialArg = {listFavorite:[]};
+
+const reducer = (state, action)=>{
+
+    const newState = {...state}
+
+    switch (action.type) {
+
+        case 'ADD_FAVORITE':
+            
+        const coincidence = state.listFavorite.filter(photo => photo.id === action.payload.id)
+
+        if (!coincidence[0]) {
+            newState.listFavorite = [...newState.listFavorite, action.payload]
+        }
+            break;
+
+        case 'DELETE_FAVORITE':
+            
+        newState.listFavorite =  newState.listFavorite.filter(photo => photo.id !== action.payload.id)
+  
+        break;
+
+        default:
+            break;
+    }
+    return newState
+}
+
+
+
 const ImageBrowser = () => {
 
     const API_URL = 'https://api.pexels.com/v1/search?query=';
@@ -11,6 +42,9 @@ const ImageBrowser = () => {
 
     const [imgList, setImgList] = React.useState([])
     const [favoriteList, setFavoriteList] = React.useState([])
+    const[error, setError] = React.useState(false)
+
+    const [favList, dispatch] = React.useReducer(reducer, initialArg);
 
     const callAPI = (text) => {
 
@@ -25,7 +59,11 @@ const ImageBrowser = () => {
         fetch(finalUrl, options)
             .then(response => response.json())
             .then(data => {
+                if(data.photos[0]){
                 setImgList(data.photos)
+                }else{
+                    setError(true)
+                }
             });
     }
 
@@ -47,25 +85,28 @@ const ImageBrowser = () => {
 
     return (
         <div className="image-browser" >
+
             <div className="image-browser__controls">
                 <input ref={inputRef} type='text' placeholder='  Introduce el texto para buscar imagenes'></input>
                 <button onClick={() => callAPI(inputRef.current.value)}>Buscar imagenes</button>
                 <button>Cambiar Tema</button>
             </div>
-
-            {imgList[0] ?
+            
+            {imgList[0] ? <>
+                <h3>Resultados de la busqueda:</h3>
                 <div className="image-browser__photo-list">
-                    <h3>Listado de fotos:</h3>
                     {imgList.map(photo => <Photo key={photo.id} photo={photo} handleClick={addFavorite} />)}
                 </div>
+            </>
                 : <p>Introduce tu imagen ...</p>}
 
-            {favoriteList[0] ?
+            {favoriteList[0] ?<>
+                <h3>Favoritos:</h3>
                 <div className="image-browser__favorite-list">
-                    <h3>Favoritos:</h3>
                     {favoriteList.map(photo => <Photo key={photo.id} photo={photo} handleClick={deleteFavorite} />)}
                 </div>
-                : <p> No hay favoritos...</p>}
+            </>
+                : <p> No hay favoritos seleccionados...</p>}
 
         </div>
     )
